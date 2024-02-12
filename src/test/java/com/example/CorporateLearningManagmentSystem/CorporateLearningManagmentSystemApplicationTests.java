3ff8eb7d820b1a8f946891c0acea7147b4a745e4
malpaniraghav65 <malpaniraghav65@gmail.com>
2024-02-12 17:55:10 +0530

    Initial commit

---
+++ src/test/java/com/example/CorporateLearningManagmentSystem/CorporateLearningManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CorporateLearningManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateLearningManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
