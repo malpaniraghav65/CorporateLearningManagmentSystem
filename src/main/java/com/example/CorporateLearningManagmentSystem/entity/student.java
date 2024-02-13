8c8e6d00265433fa74d9058ae6e7ff9f96cd1e1f
malpaniraghav65 <malpaniraghav65@gmail.com>
2024-02-13 13:00:21 +0530

    inin

--- src/main/java/com/example/CorporateLearningManagmentSystem/entity/student.java
+++ src/main/java/com/example/CorporateLearningManagmentSystem/entity/student.java
@@ -2,6 +2,6 @@ package com.example.CorporateLearningManagmentSystem.entity;
 
 public class student {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("helkjbijew");
     }
 }
