35fb00d3eadee9e6e887b30379ffd7a5b6ea09fe
HetPatel220 <hetkpatel1801@gmail.com>
2024-02-14 14:53:52 +0530

    commit

--- src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
+++ src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
@@ -1,4 +1,7 @@
 package com.example.CorporateLearningManagmentSystem.controller;
 
 public class Controller {
+    public static void main(String[] args) {
+        System.out.println("wsfxfe");
+    }
 }
