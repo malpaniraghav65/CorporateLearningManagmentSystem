385b40c917f84bc36dcbd20853ee5a0d5c2b5e84
HetPatel220 <hetkpatel1801@gmail.com>
2024-02-14 18:08:11 +0530

    initial commit

--- src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
+++ src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
@@ -1,6 +1,6 @@
 package com.example.CorporateLearningManagmentSystem.controller;
 
-public class CorporateLearningController {
+public class Controller {
     public static void main(String[] args) {
 
     }
