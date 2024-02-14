b80d182e716fd3a9e73d0e3aff69065bb653545e
HetPatel220 <hetkpatel1801@gmail.com>
2024-02-14 18:20:52 +0530

    initial commit

--- src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
+++ src/main/java/com/example/CorporateLearningManagmentSystem/controller/Controller.java
@@ -1,4 +1,20 @@
 package com.example.CorporateLearningManagmentSystem.controller;
 
+import com.example.CorporateLearningManagmentSystem.service.ServiceUserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
 public class Controller {
+    public static void main(String[] args) {
+
+    }
+    @Autowired
+    public ServiceUserRole serviceUserRole;
+
+
 }
