8b362c7dded6d0fe4966f20aa07b2c89004f87ed
malpaniraghav65 <malpaniraghav65@gmail.com>
2024-02-16 10:36:07 +0530

    changes in n

--- src/main/java/com/example/CorporateLearningManagmentSystem/dto/enrolledEmployeedto.java
+++ src/main/java/com/example/CorporateLearningManagmentSystem/dto/enrolledEmployeedto.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 @Data
 public class enrolledEmployeedto {
-    Long emp_id;
+    int emp_id;
     Long course_id;
     Float Status;
     String startdate;
     String enddate;
 
-    public enrolledEmployeedto(Long emp_id, Long course_id, float status, String startdate, String enddate) {
+    public enrolledEmployeedto(int emp_id, Long course_id, float status, String startdate, String enddate) {
         this.emp_id = emp_id;
         this.course_id = course_id;
         Status = status;
