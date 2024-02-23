736de7656ad64c5f8802ac53299e716b0af1e143
meetp5484 <meetp5484@gmail.com>
2024-02-23 11:10:37 +0530

    update friday 3

--- src/main/java/com/example/CorporateLearningManagmentSystem/repository/ReportingmanagerEmployee.java
+++
@@ -1,7 +0,0 @@
-package com.example.CorporateLearningManagmentSystem.repository;
-
-import com.example.CorporateLearningManagmentSystem.entity.ReportingmanagerEmployeeMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportingmanagerEmployee extends JpaRepository<ReportingmanagerEmployeeMapping,Long> {
-}
